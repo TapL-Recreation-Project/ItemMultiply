cdf706b7350b49e067a299244613fd77ae30a019
yehudahrrs <47502993+yehudahrrs@users.noreply.github.com>
2021-02-01 15:19:04 -0500

    Added bstats support
    https://bstats.org/plugin/bukkit/Damage%20Dupe/10205

--- src/main/java/me/swipez/itemmultiply/Itemmultiply.java
+++ src/main/java/me/swipez/itemmultiply/Itemmultiply.java
@@ -1,11 +1,13 @@
 package me.swipez.itemmultiply;
 
+import me.swipez.itemmultiply.bstats.Metrics;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Itemmultiply extends JavaPlugin {
     boolean gamestarted = false;
 
     public void onEnable() {
+        new Metrics(this, 10205);
         this.getServer().getPluginManager().registerEvents(new DamageEvent(this), this);
         this.getCommand("dupechallenge").setExecutor(new StartGame(this));
     }
